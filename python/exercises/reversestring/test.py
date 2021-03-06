bc554d4829aa23c8e5e93be1e5284af5fc6d539b
Shea Meyers <shea.meyers@gmail.com>
2021-03-06 17:05:49 +0100

    Update folder name

--- python/exercises/reversestring/test.py
+++ python/exercises/reversestring/test.py
@@ -3,8 +3,11 @@ from .main import reverse_string
 
 class TestReverseString(unittest.TestCase):
 
-    def test_reverse_string_returns_None(self):
-        self.assertIsNone(reverse_string('abcd'))
+    def test_reverses_string(self):
+        self.assertEqual(reverse_string('abcd'), 'dcba')
+    
+    def test_reverses_string_with_spaces(self):
+        self.assertEqual(reverse_string('  abcd'), 'dcba  ')
 
 if __name__ == '__main__':
     unittest.main()
