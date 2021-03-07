f1ab3dfa927cb7f7d8a9b8da21ee3952a599ba3d
Shea Meyers <shea.meyers@gmail.com>
2021-03-07 17:41:18 +0100

    Reverse a string

--- javascript/exercises/reversestring/index.js
+++ javascript/exercises/reversestring/index.js
@@ -6,6 +6,31 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    const stringLength = str.length;
+    let reversedString = '';
+
+    for(let i=1; i<=stringLength; i++) {
+        reversedString = reversedString + str[stringLength-i];
+    }
+
+    // for (let character of str) {
+    //     reversedString = character + reversedString;
+    // }
+
+    return reversedString;
+}
+
+// function reverse(str) {
+//     return str.split('').reduce((rev, char) => char + rev, '');
+// }
+
+// function reverse(str) {
+//     // return str.split('').reverse().join('');
+//     const array = str.split('');
+//     array.reverse()
+//     return array.join('');
+// }
+
 
 module.exports = reverse;
--- python/exercises/reversestring/main.py
+++ python/exercises/reversestring/main.py
@@ -1,2 +1,12 @@
 def reverse_string(string):
-    return None
+
+    string_length = len(string)
+    reversed_string = ''
+
+    for i in range(1, string_length+1):
+        reversed_string += string[string_length-i]
+
+    return reversed_string
+
+# def reverse_string(string):
+#     return string[::-1]
