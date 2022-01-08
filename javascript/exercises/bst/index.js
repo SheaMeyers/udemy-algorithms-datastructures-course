c4c0b53b4f80b39d6863421d8ccd800b1226a2dd
Shea Meyers <shea.meyers@gmail.com>
2022-01-08 15:19:58 +0100

    Implement binary search trees

--- javascript/exercises/bst/index.js
+++ javascript/exercises/bst/index.js
@@ -11,6 +11,47 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+    insert(data) {
+        if (data < this.data) {
+            if (this.left === null) {
+                this.left = new Node(data);
+            } else {
+                this.left.insert(data);
+            }
+        } else {
+            if (this.right === null) {
+                this.right = new Node(data);
+            } else {
+                this.right.insert(data);
+            }
+        }
+    }
+
+    contains(data) {
+        if (data === this.data) {
+            return this;
+        }
+        else if (data < this.data) {
+            if (this.left === null) {
+                return null;
+            } else {
+                return this.left.contains(data);
+            }
+        } else {
+            if (this.right === null) {
+                return null;
+            } else {
+                return this.right.contains(data);
+            }
+        }
+    }
+}
 
 module.exports = Node;
